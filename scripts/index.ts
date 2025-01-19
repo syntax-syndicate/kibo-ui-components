62408c9c229bd5552cb2c6e794a01eadcb1803e6
Hayden Bleasel <hello@haydenbleasel.com>
2025-01-19 17:09:04 -0500

    Update index.ts

--- scripts/index.ts
+++ scripts/index.ts
@@ -4,7 +4,6 @@ import { execSync } from 'node:child_process';
 import { packages } from './utils';
 
 const args = process.argv.slice(2);
-const url = 'https://www.kibo-ui.com';
 
 if (args.length !== 2 || args[0] !== 'add' || !args[1].trim()) {
   console.log('Usage: npx kibo-ui add [name]');
@@ -20,6 +19,6 @@ const packageName = args[1];
 
 console.log(`Adding ${packageName} component...`);
 
-execSync(`npx -y shadcn@latest add ${url}/registry/${packageName}.json`, {
-  stdio: 'inherit',
-});
+const url = new URL(`registry/${packageName}.json`, 'https://www.kibo-ui.com');
+
+execSync(`npx shadcn@latest add ${url.toString()}`);
