dc9591ebf598bde2efc63900f86e3097485d755f
Hayden Bleasel <hello@haydenbleasel.com>
2025-01-20 16:47:48 -0500

    48 dynamically generate registry files (#50)
    * Update build-registry.ts
    
    * Delete registry files
    
    * Fix typos
    
    * Remove unused package deps
    
    * Update registry files
    
    * Fix paths and schema
    
    * Regenerate registry

--- scripts/build-registry.ts
+++ scripts/build-registry.ts
@@ -1,8 +1,6 @@
 import { promises as fs } from 'node:fs';
 import { readdirSync } from 'node:fs';
 import path, { join } from 'node:path';
-import type { z } from 'zod';
-import type { Registry, registryItemFileSchema } from '../packages/shadcn-ui';
 
 const cwd = process.cwd();
 const packagesPath = join(cwd, 'packages');
@@ -14,9 +12,6 @@ const packages = packagesDir
   .map((dir) => dir.name);
 
 const PUBLIC_FOLDER_BASE_PATH = 'apps/docs/public/registry';
-const COMPONENT_FOLDER_PATH = 'ui';
-
-type File = z.infer<typeof registryItemFileSchema>;
 
 const writeFileRecursive = async (filePath: string, data: string) => {
   const dir = path.dirname(filePath);
@@ -25,52 +20,59 @@ const writeFileRecursive = async (filePath: string, data: string) => {
   await fs.writeFile(filePath, data, 'utf-8');
 };
 
-const getComponentFiles = async (pkg: string, files: File[]) => {
-  const cwd = process.cwd();
-  const packagesPath = join(cwd, 'packages');
-  const filesArray: File[] = [];
-
-  for (const file of files) {
-    const filePath = path.join(packagesPath, pkg, file.path);
-    const fileContent = await fs.readFile(filePath, 'utf-8');
-
-    filesArray.push({
-      type: file.type,
-      path: file.path,
-      content: fileContent,
-      target: `${COMPONENT_FOLDER_PATH}/${file.path}`,
-    });
-  }
-
-  return filesArray;
-};
-
 const buildRegistry = async (pkg: string) => {
   const cwd = process.cwd();
-  const registryPath = join(cwd, 'packages', pkg, 'registry.ts');
-  const registry: Registry = await import(registryPath).then((r) => r.ui);
 
-  // make a json file and put it in public folder
-  for (const component of registry) {
-    const { files } = component;
+  const packagePath = join(cwd, 'packages', pkg, 'package.json');
+  const packageJson = await import(packagePath);
 
-    if (!files) {
-      throw new Error('No files found for component');
-    }
+  const dependencies = Object.keys(packageJson.dependencies).filter(
+    (dep) => !['react', 'react-dom', '@repo/shadcn-ui'].includes(dep)
+  );
 
-    const filesArray = await getComponentFiles(pkg, files);
+  const devDependencies = Object.keys(packageJson.devDependencies).filter(
+    (dep) =>
+      ![
+        '@repo/typescript-config',
+        '@types/react',
+        '@types/react-dom',
+        'typescript',
+      ].includes(dep)
+  );
 
-    const json = JSON.stringify(
-      {
-        ...component,
-        files: filesArray,
-      },
-      null,
-      2
-    );
-    const jsonPath = `${PUBLIC_FOLDER_BASE_PATH}/${pkg}.json`;
-    await writeFileRecursive(jsonPath, json);
-  }
+  const contentPath = join(cwd, 'packages', pkg, 'index.tsx');
+  const content = await fs.readFile(contentPath, 'utf-8');
+
+  const registryDependencies = (
+    content.match(/@\/components\/ui\/([a-z-]+)/g) || []
+  )
+    .map((path) => path.split('/').pop())
+    .filter((name): name is string => !!name);
+
+  const json = JSON.stringify(
+    {
+      $schema: 'https://ui.shadcn.com/schema/registry.json',
+      homepage: `https://www.kibo-ui.com/${pkg}`,
+      name: pkg,
+      type: 'registry:ui',
+      author: 'Hayden Bleasel <hello@haydenbleasel.com>',
+      registryDependencies,
+      dependencies,
+      devDependencies,
+      files: [
+        {
+          type: 'registry:ui',
+          path: 'index.tsx',
+          content,
+          target: `kibo-ui/${pkg}.tsx`,
+        },
+      ],
+    },
+    null,
+    2
+  );
+  const jsonPath = `${PUBLIC_FOLDER_BASE_PATH}/${pkg}.json`;
+  await writeFileRecursive(jsonPath, json);
 };
 
 const main = async () => {
