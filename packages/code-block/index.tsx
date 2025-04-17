e7466c9039294350a54df7aa43ea69345841e772
Hayden Bleasel <hello@haydenbleasel.com>
2025-04-16 21:25:42 -0700

    Resolves #108

--- packages/code-block/index.tsx
+++ packages/code-block/index.tsx
@@ -89,7 +89,12 @@ import {
   transformerNotationWordHighlight,
 } from '@shikijs/transformers';
 import { CheckIcon, CopyIcon } from 'lucide-react';
-import type { ComponentProps, HTMLAttributes, ReactElement } from 'react';
+import type {
+  ComponentProps,
+  HTMLAttributes,
+  ReactElement,
+  ReactNode,
+} from 'react';
 import {
   cloneElement,
   createContext,
@@ -179,20 +184,134 @@ const filenameIconMap = {
   '*.wasm': SiWebassembly,
 };
 
+const lineNumberClassNames = cn(
+  '[&_code]:[counter-reset:line]',
+  '[&_code]:[counter-increment:line_0]',
+  '[&_.line]:before:content-[counter(line)]',
+  '[&_.line]:before:inline-block',
+  '[&_.line]:before:[counter-increment:line]',
+  '[&_.line]:before:w-4',
+  '[&_.line]:before:mr-4',
+  '[&_.line]:before:text-[13px]',
+  '[&_.line]:before:text-right',
+  '[&_.line]:before:text-muted-foreground/50',
+  '[&_.line]:before:font-mono',
+  '[&_.line]:before:select-none'
+);
+
+const darkModeClassNames = cn(
+  'dark:[&_.shiki]:!text-[var(--shiki-dark)]',
+  'dark:[&_.shiki]:!bg-[var(--shiki-dark-bg)]',
+  'dark:[&_.shiki]:![font-style:var(--shiki-dark-font-style)]',
+  'dark:[&_.shiki]:![font-weight:var(--shiki-dark-font-weight)]',
+  'dark:[&_.shiki]:![text-decoration:var(--shiki-dark-text-decoration)]',
+  'dark:[&_.shiki_span]:!text-[var(--shiki-dark)]',
+  'dark:[&_.shiki_span]:![font-style:var(--shiki-dark-font-style)]',
+  'dark:[&_.shiki_span]:![font-weight:var(--shiki-dark-font-weight)]',
+  'dark:[&_.shiki_span]:![text-decoration:var(--shiki-dark-text-decoration)]'
+);
+
+const lineHighlightClassNames = cn(
+  '[&_.line.highlighted]:bg-blue-50',
+  '[&_.line.highlighted]:after:bg-blue-500',
+  '[&_.line.highlighted]:after:absolute',
+  '[&_.line.highlighted]:after:left-0',
+  '[&_.line.highlighted]:after:top-0',
+  '[&_.line.highlighted]:after:bottom-0',
+  '[&_.line.highlighted]:after:w-0.5',
+  'dark:[&_.line.highlighted]:!bg-blue-500/10'
+);
+
+const lineDiffClassNames = cn(
+  '[&_.line.diff]:after:absolute',
+  '[&_.line.diff]:after:left-0',
+  '[&_.line.diff]:after:top-0',
+  '[&_.line.diff]:after:bottom-0',
+  '[&_.line.diff]:after:w-0.5',
+  '[&_.line.diff.add]:bg-emerald-50',
+  '[&_.line.diff.add]:after:bg-emerald-500',
+  '[&_.line.diff.remove]:bg-rose-50',
+  '[&_.line.diff.remove]:after:bg-rose-500',
+  'dark:[&_.line.diff.add]:!bg-emerald-500/10',
+  'dark:[&_.line.diff.remove]:!bg-rose-500/10'
+);
+
+const lineFocusedClassNames = cn(
+  '[&_code:has(.focused)_.line]:blur-[2px]',
+  '[&_code:has(.focused)_.line.focused]:blur-none'
+);
+
+const wordHighlightClassNames = cn(
+  '[&_.highlighted-word]:bg-blue-50',
+  'dark:[&_.highlighted-word]:!bg-blue-500/10'
+);
+
+const codeBlockClassName = cn(
+  'mt-0 text-sm',
+  '[&_pre]:py-4',
+  '[&_.shiki]:!bg-[var(--shiki-bg)]',
+  '[&_code]:w-full',
+  '[&_code]:grid',
+  '[&_code]:overflow-x-auto',
+  '[&_code]:bg-transparent',
+  '[&_.line]:px-4',
+  '[&_.line]:w-full',
+  '[&_.line]:relative'
+);
+
+const highlight = (
+  html: string,
+  language?: BundledLanguage,
+  themes?: CodeOptionsMultipleThemes['themes']
+) =>
+  codeToHtml(html, {
+    lang: language ?? 'typescript',
+    themes: themes ?? {
+      light: 'github-light',
+      dark: 'github-dark-default',
+    },
+    transformers: [
+      transformerNotationDiff({
+        matchAlgorithm: 'v3',
+      }),
+      transformerNotationHighlight({
+        matchAlgorithm: 'v3',
+      }),
+      transformerNotationWordHighlight({
+        matchAlgorithm: 'v3',
+      }),
+      transformerNotationFocus({
+        matchAlgorithm: 'v3',
+      }),
+      transformerNotationErrorLevel({
+        matchAlgorithm: 'v3',
+      }),
+    ],
+  });
+
+type CodeBlockData = {
+  language: string;
+  filename: string;
+  code: string;
+};
+
 type CodeBlockContextType = {
   value: string | undefined;
   onValueChange: ((value: string) => void) | undefined;
+  data: CodeBlockData[];
 };
 
 const CodeBlockContext = createContext<CodeBlockContextType>({
   value: undefined,
   onValueChange: undefined,
+  data: [],
 });
 
 export type CodeBlockProps = HTMLAttributes<HTMLDivElement> & {
   defaultValue?: string;
   value?: string;
   onValueChange?: (value: string) => void;
+  data: CodeBlockData[];
 };
 
 export const CodeBlock = ({
@@ -200,6 +319,7 @@ export const CodeBlock = ({
   onValueChange: controlledOnValueChange,
   defaultValue,
   className,
+  data,
   ...props
 }: CodeBlockProps) => {
   const [value, onValueChange] = useControllableState({
@@ -209,7 +329,7 @@ export const CodeBlock = ({
   });
 
   return (
-    <CodeBlockContext.Provider value={{ value, onValueChange }}>
+    <CodeBlockContext.Provider value={{ value, onValueChange, data }}>
       <div
         className={cn('overflow-hidden rounded-md border', className)}
         {...props}
@@ -232,6 +352,30 @@ export const CodeBlockHeader = ({
     {...props}
   />
 );
+
+export type CodeBlockFilesProps = Omit<
+  HTMLAttributes<HTMLDivElement>,
+  'children'
+> & {
+  children: (item: CodeBlockData) => ReactNode;
+};
+
+export const CodeBlockFiles = ({
+  className,
+  children,
+  ...props
+}: CodeBlockFilesProps) => {
+  const { data } = useContext(CodeBlockContext);
+
+  return (
+    <div
+      className={cn('flex grow flex-row items-center gap-2', className)}
+      {...props}
+    >
+      {data.map(children)}
+    </div>
+  );
+};
 
 export type CodeBlockFilenameProps = HTMLAttributes<HTMLDivElement> & {
   icon?: IconType;
@@ -260,7 +404,7 @@ export const CodeBlockFilename = ({
 
   return (
     <div
-      className="flex grow items-center gap-2 bg-secondary px-4 py-1.5 text-muted-foreground text-xs"
+      className="flex items-center gap-2 bg-secondary px-4 py-1.5 text-muted-foreground text-xs"
       {...props}
     >
       {Icon && <Icon className="h-4 w-4 shrink-0" />}
@@ -298,11 +442,21 @@ export const CodeBlockSelectValue = (props: CodeBlockSelectValueProps) => (
   <SelectValue {...props} />
 );
 
-export type CodeBlockSelectContentProps = ComponentProps<typeof SelectContent>;
+export type CodeBlockSelectContentProps = Omit<
+  ComponentProps<typeof SelectContent>,
+  'children'
+> & {
+  children: (item: CodeBlockData) => ReactNode;
+};
 
-export const CodeBlockSelectContent = (props: CodeBlockSelectContentProps) => (
-  <SelectContent {...props} />
-);
+export const CodeBlockSelectContent = ({
+  children,
+  ...props
+}: CodeBlockSelectContentProps) => {
+  const { data } = useContext(CodeBlockContext);
+
+  return <SelectContent {...props}>{data.map(children)}</SelectContent>;
+};
 
 export type CodeBlockSelectItemProps = ComponentProps<typeof SelectItem>;
 
@@ -329,18 +483,19 @@ export const CodeBlockCopyButton = ({
   ...props
 }: CodeBlockCopyButtonProps) => {
   const [isCopied, setIsCopied] = useState(false);
-  const { value } = useContext(CodeBlockContext);
+  const { data, value } = useContext(CodeBlockContext);
+  const code = data.find((item) => item.language === value)?.code;
 
   const copyToClipboard = () => {
     if (
       typeof window === 'undefined' ||
       !navigator.clipboard.writeText ||
-      !value
+      !code
     ) {
       return;
     }
 
-    navigator.clipboard.writeText(value).then(() => {
+    navigator.clipboard.writeText(code).then(() => {
       setIsCopied(true);
       onCopy?.();
 
@@ -389,119 +544,23 @@ const CodeBlockFallback = ({ children, ...props }: CodeBlockFallbackProps) => (
   </div>
 );
 
-export type CodeBlockBodyProps = HTMLAttributes<HTMLDivElement> & {
-  value: string;
-  lineNumbers?: boolean;
-  syntaxHighlighting?: boolean;
+export type CodeBlockBodyProps = Omit<
+  HTMLAttributes<HTMLDivElement>,
+  'children'
+> & {
+  children: (item: CodeBlockData) => ReactNode;
 };
 
 export const CodeBlockBody = ({
-  value,
-  lineNumbers = true,
-  syntaxHighlighting = true,
   children,
   className,
   ...props
 }: CodeBlockBodyProps) => {
-  const { value: activeValue } = useContext(CodeBlockContext);
-
-  if (value !== activeValue) {
-    return null;
-  }
-
-  const lineNumberClassNames = cn(
-    '[&_code]:[counter-reset:line]',
-    '[&_code]:[counter-increment:line_0]',
-    '[&_.line]:before:content-[counter(line)]',
-    '[&_.line]:before:inline-block',
-    '[&_.line]:before:[counter-increment:line]',
-    '[&_.line]:before:w-4',
-    '[&_.line]:before:mr-4',
-    '[&_.line]:before:text-[13px]',
-    '[&_.line]:before:text-right',
-    '[&_.line]:before:text-muted-foreground/50',
-    '[&_.line]:before:font-mono',
-    '[&_.line]:before:select-none'
-  );
-
-  const darkModeClassNames = cn(
-    'dark:[&_.shiki]:!text-[var(--shiki-dark)]',
-    'dark:[&_.shiki]:!bg-[var(--shiki-dark-bg)]',
-    'dark:[&_.shiki]:![font-style:var(--shiki-dark-font-style)]',
-    'dark:[&_.shiki]:![font-weight:var(--shiki-dark-font-weight)]',
-    'dark:[&_.shiki]:![text-decoration:var(--shiki-dark-text-decoration)]',
-    'dark:[&_.shiki_span]:!text-[var(--shiki-dark)]',
-    'dark:[&_.shiki_span]:![font-style:var(--shiki-dark-font-style)]',
-    'dark:[&_.shiki_span]:![font-weight:var(--shiki-dark-font-weight)]',
-    'dark:[&_.shiki_span]:![text-decoration:var(--shiki-dark-text-decoration)]'
-  );
-
-  const lineHighlightClassNames = cn(
-    '[&_.line.highlighted]:bg-blue-50',
-    '[&_.line.highlighted]:after:bg-blue-500',
-    '[&_.line.highlighted]:after:absolute',
-    '[&_.line.highlighted]:after:left-0',
-    '[&_.line.highlighted]:after:top-0',
-    '[&_.line.highlighted]:after:bottom-0',
-    '[&_.line.highlighted]:after:w-0.5',
-    'dark:[&_.line.highlighted]:!bg-blue-500/10'
-  );
-
-  const lineDiffClassNames = cn(
-    '[&_.line.diff]:after:absolute',
-    '[&_.line.diff]:after:left-0',
-    '[&_.line.diff]:after:top-0',
-    '[&_.line.diff]:after:bottom-0',
-    '[&_.line.diff]:after:w-0.5',
-    '[&_.line.diff.add]:bg-emerald-50',
-    '[&_.line.diff.add]:after:bg-emerald-500',
-    '[&_.line.diff.remove]:bg-rose-50',
-    '[&_.line.diff.remove]:after:bg-rose-500',
-    'dark:[&_.line.diff.add]:!bg-emerald-500/10',
-    'dark:[&_.line.diff.remove]:!bg-rose-500/10'
-  );
-
-  const lineFocusedClassNames = cn(
-    '[&_code:has(.focused)_.line]:blur-[2px]',
-    '[&_code:has(.focused)_.line.focused]:blur-none'
-  );
-
-  const wordHighlightClassNames = cn(
-    '[&_.highlighted-word]:bg-blue-50',
-    'dark:[&_.highlighted-word]:!bg-blue-500/10'
-  );
-
-  const codeBlockClassName = cn(
-    'mt-0 text-sm',
-    '[&_pre]:py-4',
-    '[&_.shiki]:!bg-[var(--shiki-bg)]',
-    '[&_code]:w-full',
-    '[&_code]:grid',
-    '[&_code]:overflow-x-auto',
-    '[&_code]:bg-transparent',
-    '[&_.line]:px-4',
-    '[&_.line]:w-full',
-    '[&_.line]:relative',
-    lineHighlightClassNames,
-    lineDiffClassNames,
-    lineFocusedClassNames,
-    wordHighlightClassNames,
-    lineNumbers && lineNumberClassNames,
-    darkModeClassNames,
-    className
-  );
-
-  if (!syntaxHighlighting) {
-    return (
-      <CodeBlockFallback className={codeBlockClassName} {...props}>
-        {children}
-      </CodeBlockFallback>
-    );
-  }
+  const { data } = useContext(CodeBlockContext);
 
   return (
-    <div className={codeBlockClassName} {...props}>
-      {children}
+    <div className="" {...props}>
+      {data.map(children)}
     </div>
   );
 };
@@ -509,53 +568,62 @@ export const CodeBlockBody = ({
 export type CodeBlockContentProps = {
   themes?: CodeOptionsMultipleThemes['themes'];
   language?: BundledLanguage;
+  lineNumbers?: boolean;
+  syntaxHighlighting?: boolean;
+  value?: string;
   children: string;
+  className?: string;
 };
 
 export const CodeBlockContent = ({
   children,
   themes,
-  language = 'typescript',
+  language,
+  lineNumbers = true,
+  syntaxHighlighting = true,
+  value,
+  className,
 }: CodeBlockContentProps) => {
   const [html, setHtml] = useState<string | null>(null);
+  const { value: activeValue } = useContext(CodeBlockContext);
+  const compiledClassName = cn(
+    codeBlockClassName,
+    lineHighlightClassNames,
+    lineDiffClassNames,
+    lineFocusedClassNames,
+    wordHighlightClassNames,
+    darkModeClassNames,
+    lineNumbers && lineNumberClassNames,
+    className
+  );
 
   useEffect(() => {
-    codeToHtml(children as string, {
-      lang: language,
-      themes: themes ?? {
-        light: 'vitesse-light',
-        dark: 'vitesse-dark',
-      },
-      transformers: [
-        transformerNotationDiff({
-          matchAlgorithm: 'v3',
-        }),
-        transformerNotationHighlight({
-          matchAlgorithm: 'v3',
-        }),
-        transformerNotationWordHighlight({
-          matchAlgorithm: 'v3',
-        }),
-        transformerNotationFocus({
-          matchAlgorithm: 'v3',
-        }),
-        transformerNotationErrorLevel({
-          matchAlgorithm: 'v3',
-        }),
-      ],
-    })
+    if (value !== activeValue || !syntaxHighlighting) {
+      return;
+    }
+
+    highlight(children as string, language, themes)
       .then(setHtml)
       .catch(console.error);
-  }, [children, themes, language]);
+  }, [children, themes, activeValue, syntaxHighlighting, value, language]);
 
-  if (!html) {
-    return <CodeBlockFallback>{children}</CodeBlockFallback>;
+  if (value !== activeValue) {
+    return null;
+  }
+
+  if (!syntaxHighlighting || !html) {
+    return (
+      <CodeBlockFallback className={compiledClassName}>
+        {children}
+      </CodeBlockFallback>
+    );
   }
 
   return (
     <div
       // biome-ignore lint/security/noDangerouslySetInnerHtml: "Kinda how Shiki works"
       dangerouslySetInnerHTML={{ __html: html }}
+      className={compiledClassName}
     />
   );
 };
