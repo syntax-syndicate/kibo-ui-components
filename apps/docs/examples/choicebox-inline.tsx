f21946da735b6c132834a4401d32cc8028376ccc
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-03 14:01:06 -0700

    Create Choicebox component

---
+++ apps/docs/examples/choicebox-inline.tsx
@@ -0,0 +1,47 @@
+import {
+  Choicebox,
+  ChoiceboxItem,
+  ChoiceboxItemContent,
+  ChoiceboxItemDescription,
+  ChoiceboxItemHeader,
+  ChoiceboxItemIndicator,
+  ChoiceboxItemTitle,
+} from '@repo/choicebox';
+
+const options = [
+  {
+    id: '1',
+    label: 'Option 1',
+    description: 'This is the first option',
+  },
+  {
+    id: '2',
+    label: 'Option 2',
+    description: 'This is the second option',
+  },
+];
+
+const Example = () => (
+  <Choicebox
+    defaultValue="1"
+    style={{
+      gridTemplateColumns: `repeat(${options.length}, 1fr)`,
+    }}
+  >
+    {options.map((option) => (
+      <ChoiceboxItem value={option.id} key={option.id}>
+        <ChoiceboxItemHeader>
+          <ChoiceboxItemTitle>{option.label}</ChoiceboxItemTitle>
+          <ChoiceboxItemDescription>
+            {option.description}
+          </ChoiceboxItemDescription>
+        </ChoiceboxItemHeader>
+        <ChoiceboxItemContent>
+          <ChoiceboxItemIndicator />
+        </ChoiceboxItemContent>
+      </ChoiceboxItem>
+    ))}
+  </Choicebox>
+);
+
+export default Example;
---
+++ apps/docs/examples/choicebox.tsx
@@ -0,0 +1,48 @@
+import {
+  Choicebox,
+  ChoiceboxItem,
+  ChoiceboxItemContent,
+  ChoiceboxItemDescription,
+  ChoiceboxItemHeader,
+  ChoiceboxItemIndicator,
+  ChoiceboxItemSubtitle,
+  ChoiceboxItemTitle,
+} from '@repo/choicebox';
+
+const options = [
+  {
+    id: '1',
+    label: 'Option 1',
+    subtitle: '(the first option)',
+    description: 'This is the first option',
+  },
+  {
+    id: '2',
+    label: 'Option 2',
+    subtitle: '(the second option)',
+    description: 'This is the second option',
+  },
+];
+
+const Example = () => (
+  <Choicebox defaultValue="1">
+    {options.map((option) => (
+      <ChoiceboxItem value={option.id} key={option.id}>
+        <ChoiceboxItemHeader>
+          <ChoiceboxItemTitle>
+            {option.label}
+            <ChoiceboxItemSubtitle>{option.subtitle}</ChoiceboxItemSubtitle>
+          </ChoiceboxItemTitle>
+          <ChoiceboxItemDescription>
+            {option.description}
+          </ChoiceboxItemDescription>
+        </ChoiceboxItemHeader>
+        <ChoiceboxItemContent>
+          <ChoiceboxItemIndicator />
+        </ChoiceboxItemContent>
+      </ChoiceboxItem>
+    ))}
+  </Choicebox>
+);
+
+export default Example;
