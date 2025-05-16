8a98cca660964163ed58d1e9800dfd8a8f69e2cc
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-16 11:07:37 -0700

    Create Comparison component

---
+++ apps/docs/examples/comparison-event-handlers.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { Comparison, ComparisonHandle, ComparisonItem } from '@repo/comparison';
+import Image from 'next/image';
+
+const Example = () => (
+  <Comparison
+    className="aspect-video"
+    onDragStart={() => console.log('drag start')}
+    onDragEnd={() => console.log('drag end')}
+  >
+    <ComparisonItem position="left" className="bg-red-500">
+      <Image
+        src="https://placehold.co/1920x1080?random=1"
+        alt="Placeholder 1"
+        className="opacity-50"
+        width={1920}
+        height={1080}
+        unoptimized
+      />
+    </ComparisonItem>
+    <ComparisonItem position="right" className="bg-blue-500">
+      <Image
+        src="https://placehold.co/2560x1440?random=2"
+        alt="Placeholder 2"
+        className="opacity-50"
+        width={2560}
+        height={1440}
+        unoptimized
+      />
+    </ComparisonItem>
+    <ComparisonHandle />
+  </Comparison>
+);
+
+export default Example;
